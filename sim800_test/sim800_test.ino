bd4b37c4a7746e34191553c12b19fd50c92e448e
Saksham goyal <spikee.sakshu@gmail.com>
2017-10-31 17:34:40 +0530

    Add files via upload

---
+++ sim800_test/sim800_test.ino
@@ -0,0 +1,16 @@
+/* Developed by Spikee */
+
+void setup() {
+// initialize serial communications at 9600 bps:
+Serial.begin(9600);
+
+delay(10000);
+// print the serial data to GSM
+Serial.print("ATD**********;" ); // replace "*" with the mobile no. to get a call using SIM800
+// wait 10 seconds before the next loop
+delay(10000);
+Serial.print("ATH" ); // hold the call
+}
+
+void loop() {
+}
